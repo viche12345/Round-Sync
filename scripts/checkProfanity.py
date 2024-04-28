f1dfca6447637c87b490d160f6bbb312a297e8d9
Felix Nüsse <felix.nuesse@t-online.de>
2024-04-28 10:58:59 +0200

    make result more helpful

--- scripts/checkProfanity.py
+++ scripts/checkProfanity.py
@@ -11,7 +11,7 @@ for line in lines:
     print("Checking: "+str(line.rstrip()))
     prediction = predict_prob([line.rstrip()])
     if prediction[0] > 0.5:
-      file.write(str(line.rstrip())+"\n")
+      file.write(str(prediction[0]) + " - " + str(line.rstrip())+"\n")
       print("Offending line: "+str(line.rstrip()))
 
 file.close()
