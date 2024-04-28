c9e3239c732f2b1fc30ce572e058cd0197cfe881
Felix Nüsse <felix.nuesse@t-online.de>
2024-04-28 11:41:58 +0200

    filter generated translation list

--- scripts/generateFilelist.sh
+++ scripts/generateFilelist.sh
@@ -8,8 +8,15 @@ fi
 DIFF=$(git diff -U0 HEAD~$1 ${@:2} | grep -E "^\+" | grep -v +++ | cut -c 2- | sed 's/^[ \t]*\(.*$\)/\1/')
 echo "<xml>$DIFF</xml>" | xmlstarlet sel -t -m '//string' -v . -n > changed_texts.txt
 TRANSLATIONS=$(cat changed_texts.txt)
-TRANSLATIONS="${TRANSLATIONS//'%'/' ; '}"
+
+
 TRANSLATIONS="${TRANSLATIONS//$'\n'/' ; '}"
 TRANSLATIONS="${TRANSLATIONS//$'\r'/' ; '}"
-TRANSLATIONS="${TRANSLATIONS//$'\$'/''}"
+
+# First, replace all string-substitutions
+TRANSLATIONS=$(echo $TRANSLATIONS | sed 's/%1$s//g')
+TRANSLATIONS=$(echo $TRANSLATIONS | sed 's/%d//g')
+TRANSLATIONS=$(echo $TRANSLATIONS | sed 's/%s//g')
+TRANSLATIONS=$(echo $TRANSLATIONS | sed 's/"//g')
+
 echo $TRANSLATIONS
